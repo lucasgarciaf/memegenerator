c0c8dff1ad9745decf4aaa4bbd80aad0499d7dc2
lucasgarciaf <lucs.rioiv@gmail.com>
2022-09-09 10:47:13 -0300

    updating repo

--- memegenerator/src/components/Box.js
+++ memegenerator/src/components/Box.js
@@ -1,15 +1,9 @@
 import { useState } from "react";
 
-export default function Box(props) {
-  const [on, setOn] = useState(props.on);
-
+export default function Box() {
   const styles = {
     backgroundColor: on ? "#222222" : "transparent",
   };
 
-  function toggle() {
-    setOn((prevOn) => !prevOn);
-  }
-
-  return <div style={styles} className="box" onClick={toggle}></div>;
+  return <div style={styles} className="box"></div>;
 }
--- memegenerator/src/components/Meme.js
+++ memegenerator/src/components/Meme.js
@@ -7,8 +7,24 @@ import { useState } from "react";
 export default function Meme() {
   const [squares, setSquares] = useState(boxes);
 
+  function toggle(id) {
+    setSquares((prevSquares) => {
+      const newSquares = [];
+      for (let i = 0; i < prevSquares.lentgth; i++) {
+        const currentSquare = prevSquares[i];
+        if (currentSquare.id === id) {
+          const updatedSquare = { ...currentSquare, on: !currentSquare.on };
+          newSquares.push(updatedSquare);
+        } else {
+          newSquares.push(currentSquare);
+        }
+      }
+      return newSquares;
+    });
+  }
+
   const squareElements = squares.map((square) => (
-    <Box key={square.id} on={square.on} />
+    <Box key={square.id} on={square.on} toggle={toggle} />
   ));
 
   function handleClick() {
